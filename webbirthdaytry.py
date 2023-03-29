f978cb83171f0b2918cb6892b067be0237b2c10e
septi-anita <121602448+septi-anita@users.noreply.github.com>
2023-03-29 16:39:17 +0700

    Add files via upload

---
+++ webbirthdaytry.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+
+st.set_page_config(page_title="300300", page_icon="🧊", initial_sidebar_state="expanded", layout="wide")#, theme="dark")
+
+#audio_file = open('myaudio.ogg', 'rb')
+#audio_bytes = audio_file.read()
+
+#st.audio(audio_bytes, format='audio/ogg')
+
+#image = Image.open('sunrise.jpg')
+
+#st.image(image, caption="I'm one of the luckiest person cause I have friend like u")
+st.balloons()
+st.title(" ***")
+st.write("Happy Birthday to u")
+tombol=st.button("pencet sini")
+if tombol:
+ st.balloons()
+ 
+
+
+
