14d8cd73dc377e04133da768de29febd7d867e34
septi-anita <121602448+septi-anita@users.noreply.github.com>
2023-05-30 15:44:12 +0700

    Update prediksihujan.py

--- prediksihujan.py
+++ prediksihujan.py
@@ -2,6 +2,7 @@ import streamlit as st
 #from PIL import Image
 import numpy as np
 import pandas as pd
+from datetime import datetime 
 #import calendar as cld
 
 st.set_page_config(page_title="300300", page_icon="🧊", initial_sidebar_state="expanded", layout="wide")#, theme="dark")
@@ -15,6 +16,8 @@ st.write("Developed by ")
 
 #input date
 date=st.date_input("Pick a Date")
+convert= date.strftime("%Y")
+st.write(convert)
 #a=date[0:3]
 #d=date[3]
 
