f1d525c6d484b70197fd1fdb935dbc5e0d4868ad
septi-anita <121602448+septi-anita@users.noreply.github.com>
2023-05-30 12:22:53 +0700

    Update prediksihujan.py

--- prediksihujan.py
+++ prediksihujan.py
@@ -20,12 +20,20 @@ st.write("Developed by ")
 date=st.date_input("Pick a Date")
 
 #input place
+###########################################
+df = pd.read_csv(r'data_hujan.csv')
+kt = df[1]
+kota = st.selectbox ("Pilih lokasi anda:", kt)
+
+###########################################
 #df = pd.read_csv(r'.csv')
 #place = st.selectbox ("Pilih lokasi anda:", df)
 
 tombol=st.button("cari")
 if tombol:
   st.snow ()
+  CH= df[1][kota]
+  st.write (CH)
  ####
  
 
