949036185253e3d24c2e54f32b8f5eecb0285545
septi-anita <121602448+septi-anita@users.noreply.github.com>
2023-06-08 14:16:20 +0700

    Update prediksihujan.py

--- prediksihujan.py
+++ prediksihujan.py
@@ -8,20 +8,21 @@ from datetime import datetime
 st.set_page_config(page_title="300300", page_icon="🧊", initial_sidebar_state="expanded", layout="wide")#, theme="dark")
 
 #Gambar
-col1, col2, = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     st.title("Rainfall Prediction AI")
     st.write("Developed by Z.L.D.S Team")
 
 with col2:
+    st.write(' ')
+    
+with col3:
     image = Image.open('Rainy Weather (HD).png')
     st.image(image, caption=None, width=400)
 
-#with col3:
-    #st.write(' ')
 
-    #input date
+#input date
 date=st.date_input("Pick a Date")
 
 Y= date.strftime("%Y")
