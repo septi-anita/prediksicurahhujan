bd1e5a2ebb195a2deb6dc6f03d2d37857bd8c267
septi-anita <121602448+septi-anita@users.noreply.github.com>
2023-06-08 15:05:25 +0700

    Update prediksihujan.py

--- prediksihujan.py
+++ prediksihujan.py
@@ -5,20 +5,20 @@ import pandas as pd
 from datetime import datetime 
 #import calendar as cld
 
-st.set_page_config(page_title="300300", page_icon="🧊", initial_sidebar_state="expanded", layout="wide")#, theme="dark")
+st.set_page_config(page_title="300300", page_icon="🧊", initial_sidebar_state="collapsed", layout="wide")#, theme="dark")
 
 #Gambar
-col1, col3 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     st.title("Rainfall Prediction AI")
     st.write("Developed by Z.L.D.S Team")
 
-#with col2:
-    #st.write('       ')
+with col2:
+    st.write('       ')
     
 with col3:
-    image = Image.open('Umbrella.png')
+    image = Image.open('Rainy Weather (HD).png')
     st.image(image, caption=None, width=420)
 
 
