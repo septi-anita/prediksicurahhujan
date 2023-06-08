580d36f193aa754c288cfdc66a764f168b083331
septi-anita <121602448+septi-anita@users.noreply.github.com>
2023-06-08 15:42:34 +0700

    Update prediksihujan.py

--- prediksihujan.py
+++ prediksihujan.py
@@ -19,7 +19,7 @@ with col2:
     
 with col3:
     image = Image.open('Rainy Weather (HD).png')
-    st.image(image, caption=None, width=340)
+    st.image(image, caption=None, width=320)
 
 
 #input date
