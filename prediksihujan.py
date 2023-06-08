f5ed352c96e8f6720d1ff1538b6511c7c2b08115
septi-anita <121602448+septi-anita@users.noreply.github.com>
2023-06-08 12:12:03 +0700

    Update prediksihujan.py

--- prediksihujan.py
+++ prediksihujan.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 st.set_page_config(page_title="300300", page_icon="🧊", initial_sidebar_state="expanded", layout="wide")#, theme="dark")
 
-st.title("Prediksi Curah Hujan")
+st.title("AI Prediksi Curah Hujan")
 st.write("Developed by Z.L.D.S Team")
 
 #Gambar
@@ -31,9 +31,21 @@ D=int(D)
 
 tombol=st.button("cari")
 if tombol:
-  st.snow ()
+  #st.snow ()
   st.write (date)
-
+  
+  #if :
+    image = Image.open('hujan deras.png')
+    st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.write ('hujan deras')
+  #elif :
+    image = Image.open('gerimis.png')
+    st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.write ('gerimis')
+  #else :
+    image = Image.open('tidak hujan.png')
+    st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.write ('tidak hujan')
  
 
 
