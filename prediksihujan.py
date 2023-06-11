a5c94c5d28b6cb8a9f8cec1d14b8f3c540a99fa9
septi-anita <121602448+septi-anita@users.noreply.github.com>
2023-06-11 21:46:07 +0700

    Update prediksihujan.py

--- prediksihujan.py
+++ prediksihujan.py
@@ -60,7 +60,7 @@ if tombol:
       image = Image.open('hujan deras.png')
       st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
       st.write ('hujan deras')
-   elif 168>=CH>=82:
+   elif CH<=168 and CH>=82:
       st.write(D+1)
       image = Image.open('gerimis.png')
       st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
@@ -75,7 +75,7 @@ if tombol:
       image = Image.open('hujan deras.png')
       st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
       st.write ('hujan deras')
-    elif 168>=CH>=82:
+    elif CH<=168 and CH>=82:
       st.write(D+1)
       image = Image.open('gerimis.png')
       st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
